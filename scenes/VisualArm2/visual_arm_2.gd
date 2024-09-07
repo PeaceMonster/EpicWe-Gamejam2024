717eca7561f6976397f304562db46a58212b0fa1
Kostiantyn Milkevych <mkv2510kostya@gmail.com>
2024-09-07 20:12:03 +0200

    Merged VisualArm2

--- scenes/VisualArm2/visual_arm_2.gd
+++ scenes/VisualArm2/visual_arm_2.gd
@@ -13,5 +13,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ik_target.global_position = get_global_mouse_position()
+	# stop setting target
+	#ik_target.global_position = get_global_mouse_position()
 	pass
