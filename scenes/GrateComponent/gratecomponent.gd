68582b930ad98e973bd1f407d80bb9132e6337d3
Ugilt <ugilt3@gmail.com>
2024-09-07 20:44:59 +0200

    Grate Component

--- scenes/GrateComponent/gratecomponent.gd
+++ scenes/GrateComponent/gratecomponent.gd
@@ -2,21 +2,27 @@ extends Area2D
 
 signal grate_done
 
+
+
 @export var parent : Node2D
 @export var grate_hardness : = 10.0
 @export var health : = 20
+@onready var particles : = $CPUParticles2D
+@export var particle_color : Color = Color.WHITE
 var rng = RandomNumberGenerator.new()
 var grating := false
 
+func _ready() -> void:
+	particles.color = particle_color
 
 func grate():
 	if rng.randf() < (1.0 / grate_hardness):
 		health -= 1
+		particles.emitting = true
 	
 	if health <= 0:
 		parent.queue_free()
-		emit_signal("grate_done")
-		
+
 
 
 func _on_area_entered(area: Area2D) -> void:
@@ -25,6 +31,7 @@ func _on_area_entered(area: Area2D) -> void:
 
 func _on_area_exited(area: Area2D) -> void:
 	grating = false
+
 
 func _process(delta: float) -> void:
 	if grating:
