690be8f38f4e4cff809a8625e03a5093cca6ecf8
Kostiantyn Milkevych <mkv2510kostya@gmail.com>
2024-09-08 13:08:44 +0200

    Fixed pot moved

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var pot: AnimatedSprite2D = $Pot
+@onready var pot = $Props/Pot
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
