e586550192fc05d7c996c3b3f5d55d8218c82318
Kostiantyn Milkevych <mkv2510kostya@gmail.com>
2024-09-08 11:48:42 +0200

    Arm Clamping done

--- scenes/MouseTarget/mouse_target.gd
+++ scenes/MouseTarget/mouse_target.gd
@@ -1,13 +1,31 @@
 extends Node2D
 
 @export var drag_coefficient = 5.0
+
+@onready var visual_arm_2 = $"../VisualArm2"
+@export var max_distance: float = 440
+var base_point: Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	base_point = visual_arm_2.get_base_point()
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		self.position.x += event.relative.x * (1 / drag_coefficient)
 		self.position.y += event.relative.y * ( 1 / drag_coefficient)
+		clamp_own_position()
 		
-		
+
+func clamp_own_position() -> void:
+	var offset: Vector2 = self.global_position - base_point
+	var noffset: Vector2 = offset.normalized()
+	self.position = base_point + noffset * max_distance * min(
+		(offset.length() / max_distance),
+		1
+	)
+	
+	
+	pass
--- scenes/VisualArm2/visual_arm_2.gd
+++ scenes/VisualArm2/visual_arm_2.gd
@@ -3,6 +3,10 @@ extends Node2D
 @export var ik_target: Node2D
 @onready var skeleton_2d = $Skeleton2D
 
+func get_base_point() -> Vector2:
+	var bonepose: Vector2 = skeleton_2d.get_bone_local_pose_override(0).get_origin()
+	var skelepose: Vector2 = skeleton_2d.get_global_transform().get_origin()
+	return bonepose + skelepose
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
